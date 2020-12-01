c7e5caf43178e05cb57f896bd461b8b13bb37035
Barnaby Colby <barnaby.colby@gmail.com>
2020-12-01 22:27:11 +0000

    Build the package using makepkg

--- build.py
+++ build.py
@@ -10,7 +10,12 @@ def build_kernel(version):
         print(f"Kernel version {version} is not supported.")
         return
 
+    # Change to the directory and start the build process.
+    # Use os.system because it's soooo much simpler than subprocess and we don't need any
+    # more power.
     print(f"Building kernel version {version}.")
+    os.chdir(version_directory)
+    os.system("makepkg -srf")
 
 
 def main():
