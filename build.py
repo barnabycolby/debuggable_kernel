c7b8427815d658693d6bd1180d87ab55213b836e
Barnaby Colby <barnaby.colby@gmail.com>
2020-12-01 22:18:15 +0000

    Add argparse for the version number

--- build.py
+++ build.py
@@ -1,7 +1,16 @@
 #! /usr/bin/env python3
+import argparse
+
+def build_kernel(version):
+    print(f"Building kernel version {version}.")
+
 
 def main():
-    print("Hello world!")
+    parser = argparse.ArgumentParser(description="Builds an arch package.")
+    parser.add_argument("version", help="The version of the kernel that you want to build.")
+    args = parser.parse_args()
+
+    build_kernel(args.version)
 
 
 if __name__ == "__main__":
