a6d997ec46d4ff6775b379695be6c7b9bbee4828
Barnaby Colby <barnaby.colby@gmail.com>
2020-12-01 22:21:58 +0000

    Check whether the version is supported before trying to use it

--- build.py
+++ build.py
@@ -1,7 +1,15 @@
 #! /usr/bin/env python3
 import argparse
+import os
 
 def build_kernel(version):
+    # First, check that we have the arch package for the specified version.
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    version_directory = os.path.join(script_directory, version)
+    if not os.path.isdir(version_directory):
+        print(f"Kernel version {version} is not supported.")
+        return
+
     print(f"Building kernel version {version}.")
 
 
